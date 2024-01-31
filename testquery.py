880a6bfd9fe13b3e1326f9767e9cabe7ce162ddd
Stella Thompson <stellathompson@Stellas-MacBook-Air-4.local>
2024-01-31 08:50:13 -0600

    first test query

---
+++ testquery.py
@@ -0,0 +1,196 @@
+import psycopg2
+
+# This function tests to make sure that you can connect to the database
+def test_connection():
+
+
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="panditk",
+        user="panditk",
+        password="square555cow")
+
+    if conn is not None:
+        print( "Connection Worked!" )
+    else:
+        print( "Problem with Connection" )
+
+    return None
+
+def test_query_one():
+
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="thompsons",
+        user="thompsons",
+        password="paper876stars")
+
+    cur = conn.cursor()
+
+    sql = "SELECT * FROM elections WHERE Trump16 > 0.75 ORDER BY DESC"
+    
+    cur.execute( sql )
+
+    row = cur.fetchone()
+
+    if row == None:
+        print("No counties voted for Trump by more than 75%")
+    else:
+        print("The county that voted the most for Trump is", row[0], "by", row[2])
+
+    conn.commit()
+
+# def test_query_two():
+
+#     conn = psycopg2.connect(
+#         host="localhost",
+#         port=5432,
+#         database="thompsons",
+#         user="thompsons",
+#         password="paper876stars")
+
+#     cur = conn.cursor()
+
+#     sql = " SELECT * FROM cities ORDER BY population DESC "
+    
+#     cur.execute( sql )
+
+#     row = cur.fetchone()
+
+#     if row == None:
+#         print("Something went wrong...")
+#     else:
+#         print(row[0], "has a population of", row[2], "(the biggest in the dataset).")
+
+#     conn.commit()
+
+# def test_query_three():
+
+#     conn = psycopg2.connect(
+#         host="localhost",
+#         port=5432,
+#         database="thompsons",
+#         user="thompsons",
+#         password="paper876stars")
+
+#     cur = conn.cursor()
+
+#     sql = " SELECT * FROM cities WHERE state = 'Minnesota' ORDER BY population ASC "
+    
+#     cur.execute( sql )
+
+#     row = cur.fetchone()
+
+#     if row == None:
+#         print("Something went wrong...")
+#     else:
+#         print(row[0], "has a population of", row[2], "(the smallest in Minnesota).")
+
+#     conn.commit()
+
+# def test_query_four():
+
+#     conn = psycopg2.connect(
+#         host="localhost",
+#         port=5432,
+#         database="thompsons",
+#         user="thompsons",
+#         password="paper876stars")
+
+#     cur = conn.cursor()
+
+# #North
+#     sql = " SELECT * FROM cities ORDER BY lat DESC "
+    
+#     cur.execute( sql )
+
+#     row = cur.fetchone()
+
+#     if row == None:
+#         print("Something went wrong...")
+#     else:
+#         print(row[0], "is furthest North.")
+
+# #South
+#     sql = " SELECT * FROM cities ORDER BY lat ASC "
+    
+#     cur.execute( sql )
+
+#     row = cur.fetchone()
+
+#     if row == None:
+#         print("Something went wrong...")
+#     else:
+#         print(row[0], "is furthest South.")    
+
+# #East
+#     sql = " SELECT * FROM cities ORDER BY lon DESC "
+    
+#     cur.execute( sql )
+
+#     row = cur.fetchone()
+
+#     if row == None:
+#         print("Something went wrong...")
+#     else:
+#         print(row[0], "is farthest East.")
+
+# #West
+#     sql = " SELECT * FROM cities ORDER BY lon ASC "
+    
+#     cur.execute( sql )
+
+#     row = cur.fetchone()
+
+#     if row == None:
+#         print("Something went wrong...")
+#     else:
+#         print(row[0], "is farthest West.")
+        
+#     conn.commit()
+    
+
+# def test_query_five():
+
+#     conn = psycopg2.connect(
+#         host="localhost",
+#         port=5432,
+#         database="thompsons",
+#         user="thompsons",
+#         password="paper876stars")
+
+#     cur = conn.cursor()
+#     state = input("What is the name of the state you are looking for?")
+    
+#     if len(state) == 2:
+#         sql = "SELECT * FROM abbreviations WHERE abbreviation = '" + state + "'"
+#         cur.execute(sql)
+#         row = cur.fetchone()
+#         state_name = row[0]
+#     else:
+#         state_name = state
+
+#     #if length of string is 2, then go to abbreviation table and find the state
+#     #turn the state into variable that is then used in the SQL call
+#     #else, put the state directly into the SQL command
+#     sql = " SELECT * FROM cities WHERE state = '" + state_name + "'"
+    
+#     cur.execute( sql )
+
+#     rows = cur.fetchall()
+#     total_population = 0
+#     for row in rows:
+#         total_population += row[2]
+
+
+#     if row == None:
+#         print("Something went wrong...")
+#     else:
+#         print("Total population of", state_name, "is", total_population)
+
+#     conn.commit()
+
+test_connection()
+test_query_one()
