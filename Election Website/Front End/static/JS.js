2bd5c81b7e5fe047c2172c0754d9014c45811b33
Yeseo Jeon <yeseojeon@Yeseos-MacBook-Pro.local>
2024-02-24 15:10:49 -0600

    html template

---
+++ Election Website/Front End/static/JS.js
@@ -0,0 +1,14 @@
+function surprise() {
+  the_surprise = document.getElementById("BodyColor");
+  the_surprise.style.color = "PaleVioletRed";
+  the_surprise.style.fontSize = "60px";
+  the_surprise.style.fontWeight = "bold";
+}
+
+function changeSizeAndColor() {
+  console.log("Mouse over title!");
+  the_Title = document.getElementById("Move");
+  the_Title.style.backgroundColor = "RoyalBlue";
+  the_Title.style.fontSize = "60px";
+  the_Title.style.color = "White";
+}
