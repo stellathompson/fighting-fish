b3eda3cc0ea0b51ea0f2725536970fd9db5b4a0b
luhayang <luhayang@Luhas-Laptop.local>
2024-03-04 15:06:54 -0600

    updated

--- Election Website/api.py
+++ Election Website/api.py
@@ -17,10 +17,7 @@ def load_hompage():
 
 @app.route('/pop/<state>')
 def counties(state):
-    state_name = state.upper()
-    sql = f"SELECT county FROM elections WHERE state = '{state_name}';"
-    list_of_counties = list(map(itemgetter(0), get_data(sql)))
-    return list_of_counties
+    return render_template("../new-dataset/index.html")
 
 @app.route('/aboutus')
 def aboutus_page():
@@ -49,52 +46,6 @@ def get_pie_data(state):
     sql = f"SELECT * FROM election_county WHERE state_code = '{state.upper()}';"
     data = get_data(sql)
     return jsonify(data)
-
-'''
-# Candidate-based queries
-@app.route('/<year>/<candidate>')
-def search_candidate(year, candidate):
-    conn = psycopg2.connect(host="localhost", port = 5432, database="panditk", user="panditk", password="square555cow")
-    cur = conn.cursor()
-
-    if year == 2016 and candidate == "Trump":
-        search = "trump16"
-    elif year == 2020 and candidate == "Trump":
-        search = "trump20"
-    elif year == 2016 and candidate == "Clinton":
-        search == "clinton16"
-    else:
-        search == "biden20"
-
-    search_results = search_database(conn, cur, search)
-
-    cur.close()
-    conn.close()
-
-    return render_template('results.html', output = search_results)
-
-# Searh <what> in the database
-def search_database(db_conn, db_cursor, search):
-    sql_query = "SELECT county, state, {} FROM elections WHERE {} IS NOT NULL ORDER BY {} DESC;".format(search, search, search)
-
-    # When having multiple queries in one line
-    sql_query_list = sql_query.split(';')
-    for query in sql_query_list:
-        if (len(query) > 2):
-            db_cursor.execute(query) 
-    
-    row_list = db_cursor.fetchall()
-
-    db_conn.commit()
-
-    output_str = ""
-    for row in row_list:
-        output_str = output_str + str(row) + ",   "
-        output_str = output_str + "\n"
-    
-    return output_str
-'''
-
 
 if __name__ == '__main__':
     my_port = 5137
