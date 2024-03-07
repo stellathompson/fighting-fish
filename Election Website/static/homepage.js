5799b7ad973a1695269ad4609a99261032acd037
panditk455 <panditk@carleton.edu>
2024-03-06 23:24:48 -0600

    typewriter in homepage.html

--- Election Website/static/homepage.js
+++ Election Website/static/homepage.js
@@ -21,32 +21,3 @@ function changeSizeAndColor() {
   the_Title.style.fontSize = "60px";
   the_Title.style.color = "White";
 }
-
-
-// Text you want to type out
-const typingText = `Welcome to Election Data Visualizer,
-your ultimate resource for exploring the
-2016 and 2020 elections. Discover the diverse
-voting patterns of your specific county along with
-information regarding different demographics, race,
-and candidates. Whether you're a political enthusiast, researcher, 
-or simply curious, our platform offers insightful 
-analyses to understand the dynamics of past elections 
-like never before. Explore, compare, and gain valuable 
-insights about your county by selecting your state
-and county down below.<br><br><br>`;
-
-let i = 0;
-const speed = 50; // Speed in milliseconds
-
-function typeWriter() {
-  if (i < typingText.length) {
-    document.getElementById("typeText").innerHTML += typingText.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
-
-// Call typeWriter() when the DOM is fully loaded
-document.addEventListener("DOMContentLoaded", typeWriter);
-
